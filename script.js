a1aeb6b05123733debf1ee60962912f43031afc2
git-sktch <sangeethapalanisamy2005@gmail.com>
2026-07-06 17:23:40 +0530

    simple code staarting phase

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Portfolio Loaded Successfully");
+
+const links=document.querySelectorAll("nav a");
+
+links.forEach(link=>{
+link.addEventListener("click",function(e){
+
+e.preventDefault();
+
+const target=document.querySelector(this.getAttribute("href"));
+
+target.scrollIntoView({
+behavior:"smooth"
+});
+
+});
+});
